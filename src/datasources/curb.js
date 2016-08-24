90eb2cb377aff7dfc886479ad0810c757a37c242
Brian Steere <dianoga7@3dgo.net>
2016-08-24 10:32:44 -0500

    Refactor curb to datasource. Eventually this will have multiple sources.

---
+++ src/datasources/curb.js
@@ -0,0 +1,61 @@
+import _ from 'lodash';
+import chalk from 'chalk';
+import { Curb } from 'node-curb';
+
+export class CurbDatasource {
+	constructor(config, database) {
+		this._config = config;
+		this.curb = new Curb({
+			clientId: config.clientId,
+			clientSecret: config.clientSecret,
+			logger: this._log
+		});
+		this.powerRef = database.ref('/power');
+	}
+
+	init() {
+		this.curb.init(this._config.username, this._config.password)
+			.then(profiles => {
+				_.each(profiles, profile => {
+					this._profileToFirebase(profile);
+					this._profileWatch(profile);
+				});
+			})
+			.catch(err => {
+				console.error('Horrible failure during curb.init');
+				console.error(err);
+				process.exit(1);
+			});
+	}
+
+	_profileToFirebase(profile) {
+		this._log(`Initializing ${profile.display_name} in firebase`);
+
+		const power = {};
+
+		_.each(profile.registers, val => {
+			power[val.id] = {
+				id: val.id,
+				name: val.label,
+				groups: val.groups
+			};
+		});
+
+		this.powerRef.set(power);
+	}
+
+	_profileWatch(profile) {
+		this._log(`Starting stream for ${profile.display_name}`);
+		profile.watch(data => {
+			const updates = {};
+			_.each(data.measurements, val => {
+				updates[`${val.id}/watts`] = val.value;
+			});
+			this.powerRef.update(updates);
+		});
+	}
+
+	_log(message) {
+		console.log(chalk.green('Curb: ') + message);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,70 +1,39 @@
 import path from 'path';
 import _ from 'lodash';
+import { CurbDatasource } from './datasources';
 
-import { Curb } from './curb';
-
-global.URLSearchParams =  require('url-search-params');
 const firebase = require('firebase');
 
 const paths = {
-	configDir: path.resolve(__dirname, '../', 'config/'),
-}
-
+	configDir: path.resolve(__dirname, '../', 'config/')
+};
 
 const config = require(`${paths.configDir}/config`);
 
 class QuickDashHelper {
 	constructor() {
-		this.curb = new Curb(config.curb.clientId, config.curb.clientSecret);
 		firebase.initializeApp({
-			databaseURL: "https://project-2731511947915132034.firebaseio.com/",
+			databaseURL: 'https://project-2731511947915132034.firebaseio.com/',
 			serviceAccount: `${paths.configDir}/firebase-account.json`
 		});
 
-		this.powerRef = firebase.database().ref('/power');
+		this.database = firebase.database();
+		this.sources = [];
+
+		// Setup all the datasources
+		_.each(config, (val, key) => {
+			switch (key) {
+				case 'curb':
+					this.sources.push(new CurbDatasource(val, this.database));
+					break;
+				default:
+					break;
+			}
+		});
 	}
 
 	init() {
-		this.curb.init(config.curb.username, config.curb.password)
-			.then(profiles => {
-				_.each(profiles, profile => {
-					this._profileToFirebase(profile);
-					this._profileWatch(profile);
-				});
-			})
-			.catch(err => {
-				console.error("Horrible failure during curb.init");
-				console.error(err);
-				process.exit(1);
-			});
-	}
-
-	_profileToFirebase(profile) {
-		console.log(`Initializing ${profile.display_name} in firebase`);
-
-		const power = {};
-
-		_.each(profile.registers, val => {
-			power[val.id] = {
-				id: val.id,
-				name: val.label,
-				groups: val.groups
-			};
-		});
-
-		this.powerRef.set(power);
-
-	}
-
-	_profileWatch(profile) {
-		console.log(`Starting stream for ${profile.display_name}`);
-		profile.watch(data => {
-			const updates = {};
-			_.each(data.measurements, val => {
-				updates[`${val.id}/watts`] = val.value;
-			});
-			this.powerRef.update(updates);
-		});
+		_.each(this.sources, val => val.init());
 	}
 }
 
